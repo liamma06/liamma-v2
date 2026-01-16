6a4e4855a0f578a69ba9d51b59256ac923d2bd36
liamma06 <liam.jbr.ma@gmail.com>
2026-01-16 17:05:27 -0500

    fix name

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
             <ul className="grid gap-1 text-base mb-4">
               <li className="flex items-center gap-2 pl-6 relative">
                 <div className="absolute left-0 top-[12px] w-[8px] h-[2px] bg-current opacity-60" />
-                <span className="font-medium">Engineering <AnimatedLink href="https://www.vitalearning.ca/">VITA Learning</AnimatedLink></span>
+                <span className="font-medium">Engineering <AnimatedLink href="https://www.vitalearning.ca/">Vita Learning</AnimatedLink></span>
               </li>
               <li className="flex flex-col gap-2 pl-6 relative">
                 <div className="absolute left-0 top-[12px] w-[8px] h-[2px] bg-current opacity-60" />
